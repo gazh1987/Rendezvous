9ad2dfbd6f4341a5c4318b013d53037a098fe965
Gary Healy <gary.hely2@student.dit.ie>
2016-01-09 23:50:53 +0000

    login and register

--- rendezvous/rendezvous/settings.py
+++ rendezvous/rendezvous/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
+#Rendezous
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
