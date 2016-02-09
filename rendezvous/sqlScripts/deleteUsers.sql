c3f23e2d08bbb20c72892b844d55a26e2acdcedb
Gary Healy <gary.healy2@student.dit.ie>
2016-02-09 20:41:49 +0000

    Delete deleteUsers.sql

--- rendezvous/sqlScripts/deleteUsers.sql
+++
@@ -1,6 +0,0 @@
-﻿DELETE FROM "restApi_rendezvoususers";
-DELETE FROM "authemail_signupcode";
-DELETE FROM "restApi_phonenumbers";
-
-select * from "restApi_rendezvoususers";
-select * from "authemail_signupcode";
